8e5716bf4710bf99ee7cf36a41e8bc0bd1a7e093
Kalinina Mariya <kalinina.mariya777@gmail.com>
2022-06-30 21:20:34 +0300

    init commit

--- src/test/java/ru/netology/Registration.java
+++ src/test/java/ru/netology/Registration.java
@@ -1,2 +1,15 @@
-package ru.netology;public class Registration {
+package ru.netology;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Registration {
+    private String login;
+    private String password;
+    private String status;
 }
